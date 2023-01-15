f91bc7247e7cd15679a5126c244e1657dd1bfd01
Giorgi Dogadze <g.dogadze@gmail.com>
2023-01-15 19:55:09 +0400

    feat: add dummy commit

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -15,7 +15,7 @@ export const RQSuperHeroesPage = () => {
   console.log("data", data);
   return (
     <>
-      <h2>React Query Super Heroes Page</h2>
+      <h2>React Query Super Heroes Pages</h2>
       {data?.data.map((hero) => {
         return <div key={hero.id}>{hero.name}</div>;
       })}
