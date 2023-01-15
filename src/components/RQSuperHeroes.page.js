f341a11b29d82e8bd4eb1921cb111647fa0c266d
Giorgi Dogadze <g.dogadze@gmail.com>
2023-01-15 19:53:04 +0400

    feat: initial commit

---
+++ src/components/RQSuperHeroes.page.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+const fetchSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+
+export const RQSuperHeroesPage = () => {
+  const { isLoading, data } = useQuery("super-heroes", fetchSuperHeroes);
+
+  if (isLoading) {
+    return <h2> Loading...</h2>;
+  }
+
+  console.log("data", data);
+  return (
+    <>
+      <h2>React Query Super Heroes Page</h2>
+      {data?.data.map((hero) => {
+        return <div key={hero.id}>{hero.name}</div>;
+      })}
+    </>
+  );
+};
