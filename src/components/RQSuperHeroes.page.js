8161b32532f203582dbb2a62a8f681c98f2345ee
Giorgi Dogadze <g.dogadze@gmail.com>
2023-01-16 12:59:42 +0400

    React Query Tutorial - 13 - Custom Query Hook

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,47 +1,25 @@
-import axios from "axios";
 import { useState } from "react";
-import { useQuery } from "react-query";
-
-const fetchSuperHeroes = () => {
-  return axios.get("http://localhost:4000/superheroes");
-};
+import { useSuperHeroesData } from "../hooks/useSuperHeroesData";
 
 export const RQSuperHeroesPage = () => {
-  const [refetchIntervalTime, setRefetchIntervalTime] = useState(3000);
-  const onSuccess = (data) => {
-    if (data.data.length === 4) {
-      console.log("gio");
-      setRefetchIntervalTime(false);
-    }
-    console.log("refetchIntervalTime-1", refetchIntervalTime);
-  };
+  // const [refetchIntervalTime, setRefetchIntervalTime] = useState(3000);
+  // const onSuccess = (data) => {
+  //   if (data.data.length === 4) {
+  //     console.log("gio");
+  //     setRefetchIntervalTime(false);
+  //   }
+  //   console.log("refetchIntervalTime-1", refetchIntervalTime);
+  // };
 
-  const onError = (error) => {
-    if (data.data.length === 4) {
-      setRefetchIntervalTime(false);
-    }
-  };
+  // const onError = (error) => {
+  //   if (data.data.length === 4) {
+  //     setRefetchIntervalTime(false);
+  //   }
+  // };
+  // console.log("refetchIntervalTime-2", refetchIntervalTime);
 
-  console.log("refetchIntervalTime-2", refetchIntervalTime);
-
-  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
-    "super-heroes",
-    fetchSuperHeroes,
-    {
-      // cacheTime: 5000, //5s
-      // staleTime: 10000, //10s
-      // refetchOnMount: true,
-      // refetchOnWindowFocus: true,
-      // refetchInterval: refetchIntervalTime,
-      // refetchIntervalInBackground: true,
-      // enabled: false,
-      // onSuccess: onSuccess,
-      // onError: onError,
-      select: (data) => {
-        return data.data?.map((data) => data.name);
-      },
-    }
-  );
+  const { isLoading, data, isError, error, isFetching, refetch } =
+    useSuperHeroesData();
 
   console.log("isLoading", isLoading, "isFetching", isFetching);
 
---
+++ src/hooks/useSuperHeroesData.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+
+const fetchSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+
+export const useSuperHeroesData = () => {
+  return useQuery("super-heroes", fetchSuperHeroes, {
+    // cacheTime: 5000, //5s
+    // staleTime: 10000, //10s
+    // refetchOnMount: true,
+    // refetchOnWindowFocus: true,
+    // refetchInterval: refetchIntervalTime,
+    // refetchIntervalInBackground: true,
+    // enabled: false,
+    // onSuccess: onSuccess,
+    // onError: onError,
+    select: (data) => {
+      return data.data?.map((data) => data.name);
+    },
+  });
+};
